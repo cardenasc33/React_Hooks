1caacb9bdf2c1d04627a526898db47c5185dfece
Christian Cardenas <christiancardenas@10-31-48-124.ece.uic.edu>
2019-06-19 11:21:59 -0500

    First Commit

---
+++ app/src/context/users/userReducer.js
@@ -0,0 +1,48 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import UserContext from './userContext';
+import UserReducer from './userReducer';
+import {
+    SEARCH_USERS,
+    SET_LOADING,
+    CLEAR_USERS,
+    USER_FOUND,
+    USER_NOT_FOUND,
+    NEW_USER
+} from '../types';
+
+export default (state, action) => {
+    //evaluate type
+    switch(action.type) {
+        case SEARCH_USERS:
+            return {
+                ...state,
+                users: action.payload,
+                loading: false
+            }
+        case USER_FOUND:
+            return {
+                ...state,
+                user: action.payload
+            }
+        case USER_NOT_FOUND:
+            return {
+                ...state,
+                not_found: action.payload
+            }
+        case NEW_USER:
+            return {
+                ...state,
+                not_found: action.payload
+                //users: state.users.concat(action.payload)
+            }
+        case SET_LOADING:
+            return {
+                //state is immutable, need to copy with new payload
+                ...state,
+                loading: true
+            }
+        default:
+            return state;
+    }
+}
